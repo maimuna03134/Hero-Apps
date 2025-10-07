08b6ee054964726f975a0576acfef48d6ab54f43
maimuna03134 <mj9644212@gmail.com>
2025-10-07 23:12:57 +0600

    navbar and footer section done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Root from './pages/Root/Root.jsx';
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component:Root,
+  },
+  // {
+  //   path: "/footer",
+  //   Component:Footer,
+  // },
+]);
+
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
